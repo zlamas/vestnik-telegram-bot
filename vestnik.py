a0b0ae16f2450add159c538abf7e04e081afc949
Vadim I <vadign00@gmail.com>
2023-01-06 16:37:19 +0200

    Fix more leftovers

--- vestnik.py
+++ vestnik.py
@@ -82,12 +82,12 @@ def extract_status_change(chat_member_update):
 	old_status, new_status = status_change
 	was_member = old_status in [
 		ChatMember.MEMBER,
-		ChatMember.CREATOR,
+		ChatMember.OWNER,
 		ChatMember.ADMINISTRATOR,
 	] or (old_status == ChatMember.RESTRICTED and old_is_member is True)
 	is_member = new_status in [
 		ChatMember.MEMBER,
-		ChatMember.CREATOR,
+		ChatMember.OWNER,
 		ChatMember.ADMINISTRATOR,
 	] or (new_status == ChatMember.RESTRICTED and new_is_member is True)
 
@@ -144,7 +144,7 @@ async def unsubscribe_daily(update, left_channel=False):
 
 
 def remove_user(user_id, user_name=None):
-	daily_ids.pop(user_id)
+	daily_ids.remove(user_id)
 	save_daily_list()
 	if user_name is not None:
 		logger.info(f"Removing user {user_name} ({user_id}) from messaging list")
