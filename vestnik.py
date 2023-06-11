04f1356e73be27f4a82db775fef2af0c90824a06
Vadim I <vadign00@gmail.com>
2023-06-11 18:46:12 +0200

    Set `httpx` log level to `WARNING`

--- vestnik.py
+++ vestnik.py
@@ -40,6 +40,7 @@ logging.basicConfig(
 	level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+logging.getLogger('httpx').setLevel(logging.WARNING)
 
 config = configparser.ConfigParser()
 config.read('vestnik.conf')
